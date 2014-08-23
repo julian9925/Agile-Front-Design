16e8e2568f4cb7dcc2788450e4303c8f4cf8ae5e
julian9925 <julian9925@gmail.com>
2014-08-23 12:07:36 +0000

    Agile-Front-Design Init

---
+++ client/project.js
@@ -0,0 +1,15 @@
+/*Router.configure({
+    layoutTemplate: "layout", 
+    yieldTemplates: {
+        topbar:    {to: "topbar"}, 
+        nav:       {to: "nav"}, 
+        videoShow: {to: "videoShow"}, 
+        menu:      {to: "menu"}, 
+        main_body: {to: "main_body"},
+        logo:      {to: "logo"}
+    }
+});
+
+Router.map(function () {
+    this.route("", "layout");
+});*/
