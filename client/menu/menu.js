53903b89fc17dafeec0f165fd69da666f335140b
AgileCodeCamp <agilecodecamp@gmail.com>
2014-09-06 10:27:39 +0000

    scale video when menu button pressed

--- client/menu/menu.js
+++ client/menu/menu.js
@@ -19,11 +19,13 @@ flag = true;
 
 Template.menu.events({
     "click #toggle": function (e) {
-        $("#toggle").toggleClass("on");
-        $("#menu").toggleClass("on");
+        $("#toggle").toggleClass("on", 1000);
+        $("#menu").toggleClass("on", 1000);
         if (flag) {
+            $("#player").animate({transform: "scale(.7, .7)", opacity: "0.3"});
             player.pauseVideo();
         } else {
+            $("#player").animate({transform: "scale(1.25, 1.25)", opacity: "0.7"});
             player.playVideo();
         }
         flag = !flag;
