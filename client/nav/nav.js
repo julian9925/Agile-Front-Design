08da4e0e6a9694e38d411c4368d3b2f83db5be6c
Castill <amazing5436@gmail.com>
2014-09-11 21:21:17 +0800

    refactor nag index counting logic

--- client/nav/nav.js
+++ client/nav/nav.js
@@ -2,8 +2,19 @@ Template.nav.events({
     'click .nav > a': function(event) {
         $(event.target).siblings().removeClass('active');
         $(event.target).addClass('active');
-        //$(this).siblings().removeClass('active');            
-        console.log('i am here', $(event.target));  
     }
-
 });
+
+Template.nav.rendered = function () {
+	
+	var anchors = $('.nav > a');
+
+	$(window).on('scroll', function(event) {
+
+	    var jqThis = $(this);
+	    var index = parseInt(jqThis.scrollTop() / this.innerHeight, 10);
+
+	    anchors.removeClass("active");
+	    anchors.eq(index).addClass("active");
+	});
+};
