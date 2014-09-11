81426079fb01f3a991c79084bfe2a438d29e4017
AgileCodeCamp <agilecodecamp@gmail.com>
2014-09-11 12:27:54 +0000

    add page1 background image

--- client/project.js
+++ client/project.js
@@ -1,6 +1,6 @@
 $(window).on('scroll', function(event) {
     var jqThis = $(this);
-    // console.log('page: ', jqThis.scrollTop() / this.innerHeight);
+    console.log('page: ', jqThis.scrollTop() / this.innerHeight);
     var index = parseInt(jqThis.scrollTop() / this.innerHeight, 10);
     $('.nav a').removeClass('active');
     $('.nav a:nth-child('+(index+1)+')').addClass('active');
